b57d8c09db7dd59ebb3730fdc57c135739d3c662
Nicolás Carvajal <131204306+MiniNIKKITO@users.noreply.github.com>
2026-04-07 14:13:03 +0200

    Update spotify_playlist_sorter_alphabetical_reverse.js

--- spotify_playlist_sorter_alphabetical_reverse.js
+++ spotify_playlist_sorter_alphabetical_reverse.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
+const REDIRECT_URI = "https://TU-BACKEND.up.railway.app/callback";
 
 let access_token = null;
 
